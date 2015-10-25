bd9646e7a79f05dfd880ea1835514387c9bb6de5
Gabriel Monroy <gmonroy@engineyard.com>
2015-10-25 11:10:22 -0600

    ref: allow env override of HELM_CHART and HELM_WORKSPACE

--- helm/action/action.go
+++ helm/action/action.go
@@ -1,12 +1,43 @@
 package action
 
-// CachePath is the suffix for the cache.
-const CachePath = "cache"
-const CacheChartPath = "cache/charts"
+import (
+	"os"
+	"path"
+)
 
-const WorkspacePath = "workspace"
-const WorkspaceChartPath = "workspace/charts"
+// CachePath is path to repository checkout
+var CachePath string
 
+// CacheChartPath is path to charts inside the checkout
+var CacheChartPath string
+
+// WorkspacePath is path to the local workspace
+var WorkspacePath string
+
+// WorkspaceChartPath is path to charts inside the workspace
+var WorkspaceChartPath string
+
+// DefaultNS to use during kubectl command execution
 const DefaultNS = "default"
 
 var helmpaths = []string{CachePath, WorkspacePath}
+
+func init() {
+
+	envCachePath := os.ExpandEnv("$HELM_CACHE")
+	if envCachePath == "" {
+		CachePath = "cache"
+	} else {
+		CachePath = envCachePath
+	}
+	CacheChartPath = path.Join(CachePath, "charts")
+
+	envWorkspacePath := os.ExpandEnv("$HELM_WORKSPACE")
+	if envWorkspacePath == "" {
+		WorkspacePath = "workspace"
+	} else {
+		WorkspacePath = envWorkspacePath
+	}
+	WorkspaceChartPath = path.Join(WorkspacePath, "charts")
+
+}
