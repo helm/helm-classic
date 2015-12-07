fd1a529c8c324011993b3b438cf6bbbdc9a9eaea
Adam Reese <adam@reese.io>
2015-12-07 15:38:57 -0800

    ref(*): refactor kubectl to a package
    Starting to break up https://github.com/helm/helm/pull/264 into smaller chunks

---
+++ kubectl/kubectl.go
@@ -0,0 +1,43 @@
+package kubectl
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+)
+
+// Path is the path of the kubectl binary
+var Path = "kubectl"
+
+// Runner is an interface to wrap kubectl convenience methods
+type Runner interface {
+	// ClusterInfo returns Kubernetes cluster info
+	ClusterInfo() ([]byte, error)
+	// Create uploads a chart to Kubernetes
+	Create([]byte, string) ([]byte, error)
+	// Delete removes a chart from Kubernetes.
+	Delete(string, string, string) ([]byte, error)
+	// Get returns Kubernetes resources
+	Get([]byte, string) ([]byte, error)
+}
+
+// RealRunner implements Runner to execute kubectl commands
+type RealRunner struct{}
+
+// PrintRunner implements Runner to return a []byte of the command to be executed
+type PrintRunner struct{}
+
+// Client stores the instance of Runner
+var Client Runner = RealRunner{}
+
+func commandToString(cmd *exec.Cmd) string {
+	var stdin string
+
+	if cmd.Stdin != nil {
+		b, _ := ioutil.ReadAll(cmd.Stdin)
+		stdin = fmt.Sprintf("< %s", string(b))
+	}
+
+	return fmt.Sprintf("[CMD] %s %s", strings.Join(cmd.Args, " "), stdin)
+}
