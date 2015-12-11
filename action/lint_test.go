255ff6bde7d6deac2a25723be6657cfb05001826
Josh Lane <jlane@engineyard.com>
2015-12-11 14:36:00 -0800

    test(lint): verify bad Chart.yaml errors

--- action/lint_test.go
+++ action/lint_test.go
@@ -2,10 +2,13 @@ package action
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
+
+	"gopkg.in/yaml.v2"
 )
 
 func TestLintSuccess(t *testing.T) {
@@ -75,6 +78,32 @@ func TestLintAllNone(t *testing.T) {
 	})
 
 	expectContains(t, output, fmt.Sprintf("Could not find any charts in \"%s", tmpHome))
+}
+
+func TestLintEmptyChartYaml(t *testing.T) {
+	tmpHome := createTmpHome()
+	fakeUpdate(tmpHome)
+
+	chartName := "badChart"
+
+	Create(chartName, tmpHome)
+
+	badChartYaml, _ := yaml.Marshal(make(map[string]string))
+
+	chartYaml := filepath.Join(tmpHome, WorkspaceChartPath, chartName, "Chart.yaml")
+
+	os.Remove(chartYaml)
+	ioutil.WriteFile(chartYaml, badChartYaml, 0644)
+
+	output := capture(func() {
+		Lint(chartName, tmpHome)
+	})
+
+	expectContains(t, output, "Missing Name specification in Chart.yaml file")
+	expectContains(t, output, "Missing Version specification in Chart.yaml file")
+	expectContains(t, output, "Missing description in Chart.yaml file")
+	expectContains(t, output, "Missing maintainers information in Chart.yaml file")
+	expectContains(t, output, fmt.Sprintf("Chart [%s] failed some checks", chartName))
 }
 
 func TestLintAll(t *testing.T) {
