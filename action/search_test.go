c1d13be5bca1da7bee98aa51304ed84328ac2c82
Josh Lane <jlane@engineyard.com>
2015-12-11 10:07:16 -0800

    ref(search): use new helpers for better assertions

--- action/search_test.go
+++ action/search_test.go
@@ -1,44 +1,28 @@
 package action
 
 import (
-	"io/ioutil"
-	"os"
 	"strings"
 	"testing"
-
-	"github.com/helm/helm/log"
 )
 
 func TestSearch(t *testing.T) {
 	tmpHome := createTmpHome()
 	fakeUpdate(tmpHome)
 
-	term := "homeslice"
-
-	Search(term, tmpHome, false)
+	Search("homeslice", tmpHome, false)
 }
 
 func TestSearchNotFound(t *testing.T) {
 	tmpHome := createTmpHome()
 	fakeUpdate(tmpHome)
 
-	term := "nonexistent"
-
-	// capture stdout for testing
-	old := log.Stderr
-	r, w, _ := os.Pipe()
-	log.Stderr = w
-
-	Search(term, tmpHome, false)
-
-	// read test output and restore previous stdout
-	w.Close()
-	out, _ := ioutil.ReadAll(r)
-	log.Stderr = old
-	output := string(out[:])
+	output := capture(func() {
+		Search("nonexistent", tmpHome, false)
+	})
 
 	// test that a "no chart found" message was printed
 	txt := "No results found"
+
 	if !strings.Contains(output, txt) {
 		t.Fatalf("Expected %s to be printed, got %s", txt, output)
 	}
