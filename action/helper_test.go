cf59c41e1bfc0657722e5fe0db17e260728adf32
Josh Lane <jlane@engineyard.com>
2015-12-11 10:06:44 -0800

    ref(test): add expect and capture test functions

--- action/helper_test.go
+++ action/helper_test.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"path/filepath"
 	"reflect"
+	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/helm/helm/log"
@@ -49,4 +51,47 @@ func expect(t *testing.T, a interface{}, b interface{}) {
 	if a != b {
 		t.Errorf("\n[Expected] type: %v\n%v\n[Got] type: %v\n%v\n", reflect.TypeOf(b), b, reflect.TypeOf(a), a)
 	}
+}
+
+func expectMatches(t *testing.T, actual string, expected string) {
+	regexp := regexp.MustCompile(expected)
+
+	if !regexp.Match([]byte(actual)) {
+		t.Errorf("\n[Expected] %v\nto contain %v\n", actual, expected)
+	}
+}
+
+func expectContains(t *testing.T, actual string, expected string) {
+	if !strings.Contains(actual, expected) {
+		t.Errorf("\n[Expected] %v\nto contain %v\n", actual, expected)
+	}
+}
+
+func capture(fn func()) string {
+	logStderr := log.Stderr
+	logStdout := log.Stdout
+	osStdout := os.Stdout
+	osStderr := os.Stderr
+
+	r, w, _ := os.Pipe()
+
+	log.Stderr = w
+	log.Stdout = w
+	os.Stdout = w
+	os.Stderr = w
+
+	fn()
+
+	// read test output and restore previous stdout
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+
+	defer func() {
+		log.Stderr = logStderr
+		log.Stdout = logStdout
+		os.Stdout = osStdout
+		os.Stderr = osStderr
+	}()
+
+	return string(out[:])
 }
