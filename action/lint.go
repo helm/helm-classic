6a85d349951dbee1f76a7598057c1b243694a0b1
Michelle Noorali <michellemolu@gmail.com>
2016-01-04 17:16:38 -0800

    ref(lint): better, improved, cleaner linter
    ChartValidation struct keeps track of a linting instance.
    Validation struct keeps track of a validation and its
    dependent validations as well. This will help us link validations
    in a proper format and be able to more easily see and define
    validations rather than rely on logic nested in loops and
    arbitrary functions.

--- action/lint.go
+++ action/lint.go
@@ -1,19 +1,14 @@
 package action
 
 import (
-	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 
 	"github.com/google/go-github/github"
-	"github.com/helm/helm/chart"
 	"github.com/helm/helm/log"
-	"github.com/helm/helm/manifest"
 	"github.com/helm/helm/util"
 	"github.com/helm/helm/validation"
-	"gopkg.in/yaml.v2"
 )
 
 // Owner is default Helm repository owner or organization.
@@ -53,156 +48,51 @@ func LintAll(homedir string) {
 //
 // - chartPath path to chart directory
 func Lint(chartPath string) {
-	v := new(validation.Validation)
-	chartName := filepath.Base(chartPath)
+	cv := new(validation.ChartValidation)
+	cv.Path = chartPath
 
-	//makes sure all files are in place
-	structure := directoryStructure(chartPath, v)
+	//TODO: chartPresenceValidation := v.AddError("Chart found", func(path string, v *ChartValidation) bool { }
 
-	if v.Valid() {
-		checkDirectoryStructure(structure, chartPath, v)
-	}
+	chartYamlValidation := cv.AddError("Chart.yaml is present", func(path string, v *validation.Validation) bool {
+		stat, err := os.Stat(v.ChartYamlPath())
 
-	//checks to see if chart name is unique
-	verifyChartNameUnique(chartName, v)
-	verifyMetadata(chartPath, v)
-	verifyManifests(chartPath, v)
+		return err == nil && stat.Mode().IsRegular()
+	})
 
-	numErrors := len(v.Errors)
+	chartYamlNameValidation := chartYamlValidation.AddError("Chart.yaml has a name field", func(path string, v *validation.Validation) bool {
+		chartfile, err := v.Chartfile()
 
-	if len(v.Warnings) > 0 || numErrors > 0 {
-		for _, warning := range v.Warnings {
-			log.Warn(warning)
-		}
-		for _, err := range v.Errors {
-			log.Err(err)
-		}
+		return err == nil && chartfile.Name != ""
+	})
 
-		if numErrors > 0 {
-			log.Err("Chart [%s] failed some checks", chartName)
-		} else {
-			log.Warn("Chart [%s] failed some checks", chartName)
-		}
+	chartYamlNameValidation.AddError("Name declared in Chart.yaml is the same as chart name.", func(path string, v *validation.Validation) bool {
+		chartfile, err := v.Chartfile()
+
+		return err == nil && chartfile.Name == cv.ChartName()
+
+	})
+
+	chartYamlValidation.AddError("Chart.yaml has a version field", func(path string, v *validation.Validation) bool {
+		chartfile, err := v.Chartfile()
+
+		return err == nil && chartfile.Version != ""
+	})
+
+	chartYamlValidation.AddWarning("Chart.yaml has a description field", func(path string, v *validation.Validation) bool {
+		chartfile, err := v.Chartfile()
+
+		return err == nil && chartfile.Description != ""
+	})
+
+	chartYamlValidation.AddWarning("Chart.yaml has a maintainers field", func(path string, v *validation.Validation) bool {
+		chartfile, err := v.Chartfile()
+
+		return err == nil && chartfile.Maintainers != nil
+	})
+
+	if cv.Valid() {
+		log.Info("Chart[%s] has passed all necessary checks", cv.ChartName())
 	} else {
-		log.Info("Chart [%s] has passed all necessary checks", chartName)
-	}
-}
-
-func directoryStructure(chartPath string, v *validation.Validation) map[string]os.FileInfo {
-	structure := make(map[string]os.FileInfo)
-
-	chartInfo, err := os.Stat(chartPath)
-	if err != nil {
-		v.AddError(fmt.Sprintf("Chart %s not found in workspace. Error: %v", chartPath, err))
-		return structure
-	}
-
-	if chartInfo.IsDir() {
-		files, _ := ioutil.ReadDir(chartPath)
-		for _, f := range files {
-			structure[f.Name()] = f
-		}
-	} else {
-		v.AddError(fmt.Sprintf("Chart Path [%s] is not a directory.", chartPath))
-	}
-
-	return structure
-}
-
-func checkDirectoryStructure(structure map[string]os.FileInfo, chartPath string, v *validation.Validation) {
-	if _, ok := structure["README.md"]; ok != true {
-		v.AddWarning(fmt.Sprintf("A README file was not found in %s", chartPath))
-	}
-
-	manifestInfo, ok := structure["manifests"]
-
-	if ok && manifestInfo.IsDir() {
-		// manifest files logic
-	} else {
-		v.AddError(fmt.Sprintf("A manifests directory was not found in %s", chartPath))
-	}
-}
-
-// verifyMetadata checks the Chart.yaml file for a Name, Version, Description, and Maintainers
-func verifyMetadata(chartPath string, v *validation.Validation) {
-	var y *chart.Chartfile
-
-	file := filepath.Join(chartPath, Chartfile)
-	chartDir := filepath.Base(chartPath)
-	b, err := ioutil.ReadFile(file)
-
-	if err != nil {
-		v.AddError("A Chart.yaml file was not found")
-		return
-	}
-
-	if err = yaml.Unmarshal(b, &y); err != nil {
-		v.AddError(fmt.Sprint(err))
-		return
-	}
-
-	// require name, version, description, maintaners
-	if y.Name == "" {
-		v.AddError("Missing Name specification in Chart.yaml file")
-	}
-	if y.Name != chartDir {
-		v.AddError(fmt.Sprintf("Chart.yaml name (%s) is not the same as its directory (%s)", y.Name, chartDir))
-	}
-	if y.Version == "" {
-		v.AddError("Missing Version specification in Chart.yaml file")
-	}
-	if y.Description == "" {
-		v.AddWarning("Missing description in Chart.yaml file")
-	}
-	if y.Maintainers == nil {
-		v.AddWarning("Missing maintainers information in Chart.yaml file")
-	}
-}
-
-func verifyManifests(chartPath string, v *validation.Validation) {
-	manifests, err := manifest.ParseDir(chartPath)
-
-	if err != nil {
-		v.AddError(fmt.Sprintf("Error walking manifest files. Err: %s", err))
-	}
-
-	for _, m := range manifests {
-		meta, _ := m.VersionedObject.Meta()
-		if meta.Name == "" {
-			v.AddWarning(fmt.Sprintf("missing name in %s", m.Source))
-		}
-
-		val, ok := meta.Labels["heritage"]
-		if !ok || (val != "helm") {
-			v.AddWarning(fmt.Sprintf("Missing a label: `heritage: helm` in %s", m.Source))
-		}
-
-		kind := meta.Kind
-		validKinds := InstallOrder
-
-		if valid := validKind(kind, validKinds); !valid {
-			v.AddError(fmt.Sprintf("%s is not a valid `kind` value for manifest. Here are valid kinds of manifests: %v", kind, validKinds))
-		}
-	}
-}
-
-func validKind(kind string, validKinds []string) bool {
-	for _, validKind := range validKinds {
-		if kind == validKind {
-			return true
-		}
-	}
-	return false
-}
-
-func verifyChartNameUnique(chartName string, v *validation.Validation) {
-	if RepoService == nil {
-		RepoService = github.NewClient(nil).Repositories
-	}
-
-	chartPath := filepath.Join(chartName, Chartfile)
-
-	if _, err := RepoService.DownloadContents(Owner, Project, chartPath, nil); err == nil {
-		v.AddWarning(fmt.Sprintf("Chart name %s already exists in charts repository [github.com/helm/charts]. If you're planning on submitting this chart to the charts repo, please consider changing the chart name.", chartName))
+		log.Err("Chart [%s] is not completely valid", cv.ChartName())
 	}
 }
--- validation/validation.go
+++ validation/validation.go
@@ -1,22 +1,150 @@
 package validation
 
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/helm/helm/chart"
+	"gopkg.in/yaml.v2"
+)
+
+// ChartValidation represents a specific instance of validation against a specific directory
+type ChartValidation struct {
+	Path        string
+	Validations []*Validation
+}
+
+const (
+	warningLevel = 0
+	errorLevel   = 1
+)
+
 // Validation - hold messages related to validation of something
 type Validation struct {
-	Errors   []string
-	Warnings []string
+	children  []*Validation
+	path      string
+	validator validator
+	Message   string
+	level     int
 }
 
-// AddWarning - add warning to validation
-func (v *Validation) AddWarning(w string) {
-	v.Warnings = append(v.Warnings, w)
+//ChartYamlPath - path to Chart.yaml
+func (v *Validation) ChartYamlPath() string {
+	return filepath.Join(v.path, "Chart.yaml")
 }
 
-// AddError - add error to validation
-func (v *Validation) AddError(e string) {
-	v.Errors = append(v.Errors, e)
+func (v *Validation) Chartfile() (*chart.Chartfile, error) {
+	var y *chart.Chartfile
+	b, err := ioutil.ReadFile(v.ChartYamlPath())
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = yaml.Unmarshal(b, &y); err != nil {
+		return nil, err
+	}
+
+	return y, nil
 }
 
-// Valid - return true if no errors or warnings
-func (v *Validation) Valid() bool {
-	return len(v.Errors) == 0 && len(v.Warnings) == 0
+type validator func(path string, v *Validation) (result bool)
+
+func (cv *ChartValidation) addValidator(v *Validation) {
+	cv.Validations = append(cv.Validations, v)
+}
+
+func (v *Validation) addValidator(child *Validation) {
+	v.children = append(v.children, child)
+}
+
+// AddError - add error level validation to ChartValidation
+func (cv *ChartValidation) AddError(message string, fn validator) *Validation {
+	v := new(Validation)
+	v.Message = message
+	v.validator = fn
+	v.level = errorLevel
+	v.path = cv.Path
+
+	cv.addValidator(v)
+
+	return v
+}
+
+// AddWarning - add warning level validation to ChartValidation
+func (cv *ChartValidation) AddWarning(message string, fn validator) *Validation {
+	v := new(Validation)
+	v.Message = message
+	v.validator = fn
+	v.level = warningLevel
+	v.path = cv.Path
+
+	cv.addValidator(v)
+
+	return v
+}
+
+// AddError - add error level validation to Validation
+func (v *Validation) AddError(message string, fn validator) *Validation {
+	child := new(Validation)
+	child.Message = message
+	child.validator = fn
+	child.level = errorLevel
+	child.path = v.path
+
+	v.addValidator(child)
+
+	return child
+}
+
+// AddWarning - add warning level validation to Validation
+func (v *Validation) AddWarning(message string, fn validator) *Validation {
+	child := new(Validation)
+	child.Message = message
+	child.validator = fn
+	child.level = warningLevel
+	child.path = v.path
+
+	v.addValidator(child)
+
+	return child
+}
+
+func (cv *ChartValidation) ChartName() string {
+	return filepath.Base(cv.Path)
+}
+
+func (v *Validation) valid() bool {
+	return v.validator(v.path, v)
+}
+
+func (v *Validation) walk(talker func(_ *Validation) bool) {
+	validResult := talker(v)
+
+	if validResult {
+		for _, child := range v.children {
+			child.walk(talker)
+		}
+	}
+}
+
+func (cv *ChartValidation) walk(talker func(v *Validation) bool) {
+	for _, v := range cv.Validations {
+		v.walk(talker)
+	}
+}
+
+// Valid - true if every validation passes
+func (cv *ChartValidation) Valid() bool {
+	var valid bool = true
+
+	cv.walk(func(v *Validation) bool {
+		vv := v.valid()
+		fmt.Println(fmt.Sprintf(v.Message+" : %v", vv))
+		valid = valid && vv
+		return valid
+	})
+
+	return valid
 }
