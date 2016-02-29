469957c541fbca2ea00c8e644477e889874b3f6f
Rimantas Mocevicius <rmocius@gmail.com>
2016-02-29 13:48:02 +0000

    fix(install.sh) to work on GC Shell

--- _scripts/installer/install.sh
+++ _scripts/installer/install.sh
@@ -61,11 +61,7 @@ VERSION="$(get_latest_version "${HELM_VERSION_URL}")"
 HELM_ZIP="helm-${VERSION}-${PLATFORM}-${ARCH}.zip"
 
 echo "Downloading ${HELM_ZIP} from Bintray..."
-if [[ $(which wget) ]]; then
-    wget -q "${HELM_BIN_URL_BASE}/${HELM_ZIP}"
-else
-    curl -Ls "${HELM_BIN_URL_BASE}/${HELM_ZIP}" -O
-fi
+curl -Lsk "${HELM_BIN_URL_BASE}/${HELM_ZIP}" -O
 
 echo "Extracting..."
 unzip -qo "${HELM_ZIP}"
